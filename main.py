d22f8f18743633eec9bcf8241397bc4418ab3813
Aman Gautam <97427361+aman9650@users.noreply.github.com>
2024-04-14 13:27:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #2.15.0
 #2.16.1
-
 import streamlit as st
 import numpy as np
 from tensorflow.keras.models import load_model
@@ -8,12 +7,10 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 # Load the trained model
-model = load_model('final_model .h5')
+model = load_model('final_model.h5')
 
 # Define the class labels
-class_labels = ['Actinic keratoses (akiec)', 'Basal cell carcinoma (bcc)', 'Benign keratosis-like lesions (bkl)', 'Dermatofibroma (df)', 'Melanoma (mel)', 'Melanocytic nevi (nv)', 'Melanocytic nevi (nv)']
-
-
+class_labels = ['Actinic keratoses (akiec)', 'Basal cell carcinoma (bcc)', 'Benign keratosis-like lesions (bkl)', 'Dermatofibroma (df)', 'Melanoma (mel)', 'Melanocytic nevi (nv)', 'Vascular lesions (vasc)']
 
 # Function to preprocess the uploaded image
 def preprocess_image(image):
@@ -22,13 +19,11 @@ def preprocess_image(image):
     image = np.expand_dims(image, axis=0)
     return image
 
-
 # Function to make predictions
 def predict(image):
     image = preprocess_image(image)
     predictions = model.predict(image)
     return predictions[0]
-
 
 # Streamlit app
 def main():
@@ -53,14 +48,16 @@ def main():
         st.write(f'Predicted Class: {predicted_class}')
 
         # Plot percentage of each class found
-        plt.figure(figsize=(8, 6))
-        plt.bar(class_labels, predictions * 100)
+        plt.figure(figsize=(10, 8))
+        bars = plt.bar(class_labels, predictions * 100, color=plt.cm.viridis(np.linspace(0, 1, len(class_labels))))
         plt.xlabel('Class')
         plt.ylabel('Percentage')
         plt.title('Percentage of Each Class')
-        plt.xticks(rotation=45)
+        plt.xticks([])
+        plt.grid(True)
+        legend_labels = [f'{label}: {pred*100:.2f}%' for label, pred in zip(class_labels, predictions)]
+        plt.legend(bars, legend_labels, loc='upper right', bbox_to_anchor=(1.3, 1))
         st.pyplot(plt)
-
 
 # Run the app
 if __name__ == '__main__':
